7549dfbaea30f3a9d0bf77bc7f73798ed887b9b6
ConlanSchool <conlan.school@gmail.com>
2023-02-26 19:06:28 -0800

    Add catch for errors

--- index2.js
+++ index2.js
@@ -1,5 +1,15 @@
 const { nextIssTimesForMyLocation } = require("./iss_promised");
 
+const printPassTimes = function (passTimes) {
+  for (const pass of passTimes) {
+    const dateTime = new Date(0);
+    dateTime.setUTCSeconds(pass.riseTime);
+
+    const duration = pass.duration;
+    console.log(`Next pass at ${dateTime} for ${duration} seconds!`);
+  }
+};
+
 nextIssTimesForMyLocation()
   .then((passTimes) => {
     printPassTimes(passTimes);
